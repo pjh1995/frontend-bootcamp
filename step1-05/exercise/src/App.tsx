6bfeb6af2ebeb9986208319866571635bd92b395
jhpark <qkr13wlgus57@gmail.com>
2021-07-07 01:50:45 +0900

    feat: TodoHeader, TodoListItem component

--- step1-05/exercise/src/App.tsx
+++ step1-05/exercise/src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { TodoHeader } from './components/TodoHeader';
+import React, { useState } from 'react';
+import { TodoHeader, TodoListItem } from './components';
+import { Todo } from './types';
 
-export class TodoApp extends React.Component<any, any> {
-  render() {
-    return (
-      <div>
-        <TodoHeader />
-      </div>
-    );
-  }
-}
+const TodoApp = () => {
+  console.log('reset');
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const getLastId = (): number => {
+    if (todoList.length === 0) return 0;
+    return todoList[todoList.length - 1].id;
+  };
+
+  const onAddTodo = (todo: string) => {
+    const id = getLastId();
+    console.log(todo, id);
+    setTodoList([
+      ...todoList,
+      {
+        id,
+        todo,
+      },
+    ]);
+  };
+
+  return (
+    <>
+      <TodoHeader onAddTodo={onAddTodo} />
+      <ul className="todos">
+        {todoList.map((todo) => (
+          <TodoListItem todo={todo} key={todo.id} />
+        ))}
+      </ul>
+      <footer>
+        <span>{todoList.length} items left</span>
+        <button>Clear Completed</button>
+      </footer>
+    </>
+  );
+};
+
+export default TodoApp;
--- step1-05/exercise/src/components/TodoListItem.tsx
+++ step1-05/exercise/src/components/TodoListItem.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Todo } from '../types';
 
-export class TodoListItem extends React.Component<any, any> {
-  render() {
-    return (
-      <li className="todo">
-        <label>
-          <input type="checkbox" /> Todo 1
-        </label>
-      </li>
-    );
-  }
-}
+type TodoItem = {
+  todo: Todo;
+};
+const TodoListItem = (props: TodoItem) => {
+  const { todo } = props;
+
+  return (
+    <li className="todo">
+      <label>
+        <input type="checkbox" /> {todo.todo}
+      </label>
+    </li>
+  );
+};
+
+export default TodoListItem;
